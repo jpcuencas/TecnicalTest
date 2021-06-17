a9b3d3cd5c2004ad05627b2394792349977cc0f1
jpcuencas <juan.pedro.cuenca.sevilla@gmail.com>
2021-06-17 11:48:18 +0200

    add styles

--- react-front/src/components/layout/Header.tsx
+++ react-front/src/components/layout/Header.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const styles = createUseStyles({
+    headerTitle: {
+    background:'orange',
+    color:'White',
+    borderRadius:10,
+    textAlign: 'center',
+    },
+});
 
 function Header() {
+    const classes = styles();
   return (
     <>
-    
+    <div className={classes.headerTitle}>
+        <h2>Test Lansweeper</h2>
+    </div>
     </>
   );
 }
